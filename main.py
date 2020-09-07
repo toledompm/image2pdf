1b511ac646ffedddf389a9f38c91a782285ee3a6
toledompm <toledompm@gmail.com>
2020-09-07 02:36:51 -0300

    Added default sorting function for images, added catches and error handling

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from helpers.dictListFilter import dictListFilter
 from helpers.moveInputs import moveInputs
 from helpers.pdfGen import pdfGen
 
+try:
+    from sortSettings import sortSettings                    
+except ImportError:
+    def sortSettings(elem): return elem['fileName']
+
 inputsPath = input('Enter the path to your image directory: ')
 inputsPath = inputsPath if inputsPath != '' else 'inputs'
 
 moveInputs(inputsPath)
-zipFileList = dictListFilter(getFileList('temp'),'extension','.zip')
 
-for zip in zipFileList:
-    unzip(zip['path'],'temp')
+try:
+    zipFileList = dictListFilter(getFileList('temp'),'extension','.zip')
 
-imageFileList = dictListFilter(getFileList('temp'),'extension',['.jpg','.jpeg','.png'])
+    for zip in zipFileList:
+        unzip(zip['path'],'temp')
 
-pdfGen("output",imageFileList,'')
+    imageFileList = dictListFilter(getFileList('temp'),'extension',['.jpg','.jpeg','.png'])
+    imageFileList = sorted(imageFileList, key=sortSettings)
+
+    pdfGen("output",imageFileList,'')
+except:
+    rmtree(abspath('temp'))
+    raise
 
 rmtree(abspath('temp'))
